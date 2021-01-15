ca575dcfd152e108069c2c9121d3efe74865e0a5
EGAN ALEXIS DUEÑAS MTZ <70590726+MooshroomBear@users.noreply.github.com>
2021-01-15 09:20:41 -0600

    puerto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Console } = require('console');
 const express=require('express');
 const path=require('path');
 const app=express();
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 
 app.use('/ti01',express.static(path.join(__dirname,'/public')));
 
